7d739d0ec779e7cf79294f7a02f92f5118c7e586
eunsu-source <0526eunsu@snu.ac.kr>
2026-05-15 15:43:49 +0900

    fileadd
    fileadd

---
+++ sketch.js
@@ -0,0 +1,30 @@
+let backgroundnum = 1;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  if(backgroundNum == 1){
+  }
+  if(backgroundNum == 2){
+    ellipse(30,30,30,30)
+  }
+  if(backgroundNum == 3){
+  }
+ 
+  function kePressed(){
+    if(key == '1'){
+      backgroundNum = '1'
+    }
+    if(key == '2'){
+      backgroundNum = '2'
+    }
+
+  if (key == '3'){
+    backgroundNum = '3'
+  }
+  }
+}
